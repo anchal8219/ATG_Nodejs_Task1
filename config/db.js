8c7212e145c63207e3ad4d670fca03a7c12126b6
Anchal <Anchal2011030@akgec.ac.in>
2023-12-08 12:40:21 +0530

    host api

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const mongodbUrl = "mongodb+srv://anchal123:QKWCwV1Ws0oxTXRn@cluster0.lwdemmy.mongodb.net/"
+const connDB = async () => {
+  const uri = process.env.MONGODB_URL;
 
-const connDB = ()=>{
-    return mongoose.connect(mongodbUrl)
-    // .then(() => console.log('Connected!'));
+  try {
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+};
 
-}
-
-module.exports={connDB};
-
-
+module.exports = { connDB };
